e8b0bed15fa417743542e4b02571bf90973ab125
midyantard2 <jixuanli1230@yahoo.com>
2021-01-02 16:40:35 -0500

    initial commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row">
+            <aside className="four columns footer-widgets">
+               <div className="widget widget_contact">
+
+					   <h4>Contact Me</h4>
+					   <p className="address">
+						   {name}<br />
+						   <span>{phone}</span> <br/>
+                           <span>{email}</span>
+					   </p>
+				   </div>
+
+            </aside>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Contact;
